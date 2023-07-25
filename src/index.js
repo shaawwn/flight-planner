3f0c7fb6a8857439d935db391fc1774816103c05
Shawn Lentz <shawn.lentz@gmail.com>
2023-07-25 10:20:15 -1000

    added refresh warning

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+window.onbeforeunload = function() {
+  return "Data will be lost if you leave the page, are you sure?";
+};
 root.render(
   <React.StrictMode>
     <App />
